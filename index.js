dd1fadc03c02c430fbbc333b2cab4ab9de6741bf
sysλ <hi@syslambda.fr>
2020-10-06 23:04:46 +0200

    Updated index.js
    Added bot compatibility and reset permissions. Trying things.

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const discord = require("discord.js");
 const client  = new discord.Client();
 require('dotenv').config();
 
+// "voice": "text"
 const channels = {
-    "voice": "text",
-    "633391693801979938": "762435383874813973",
+    //"633391693801979938": "762435383874813973",
     "760491026074304553": "762001054174281759",
     "624973782829760512": "625047148051955742",
     "624970279260782601": "625026392735612968"
 }
 
-client.on("ready", () => console.log("\x1b[32m%s\x1b[0m", `Ready as '${client.user.tag}' (${client.user.id}) | Created by @sysLambda (syslambda.fr) - github.com/sysLambda/Plain-of-Hyrule`));
+client.on("ready", () => {
+    console.log("\x1b[32m%s\x1b[0m", `Ready as '${client.user.tag}' (${client.user.id}) | Created by @sysLambda (syslambda.fr) - github.com/sysLambda/Plain-of-Hyrule`);
+    for(const id in channels) {
+        client.channels.cache.get(channels[id]).overwritePermissions([{id:client.channels.cache.get(channels[id]).guild.roles.everyone,deny:['VIEW_CHANNEL'], allow:['SEND_MESSAGES','READ_MESSAGE_HISTORY']}], 'Reset').then(() => {
+            client.channels.cache.get(id).guild.members.cache.filter(m => m.user.bot).map(m => client.channels.cache.get(channels[id]).createOverwrite(m.id, {VIEW_CHANNEL:true}));
+            client.channels.cache.get(id).members.map(m => client.channels.cache.get(channels[id]).createOverwrite(m.id, {VIEW_CHANNEL:true}));
+        });
+    }
+});
 
 client.on("message", (message) => {
     if(message.channel.type !== "text" || message.author.bot || !message.content.toLowerCase().startsWith("poh!")) return;
@@ -22,16 +30,11 @@ client.on("message", (message) => {
 
 client.on("voiceStateUpdate", (arg0, arg1) => {
 
-    if(arg0.channelID && !arg1.channelID && channels[arg0.channelID]) 
-        arg0.guild.channels.cache.get(channels[arg0.channelID]).createOverwrite(arg0.member.id, {
-            VIEW_CHANNEL: false
-        });
-
-    if(!arg0.channelID && arg1.channelID && channels[arg1.channelID])
+    if(arg0.channelID && !arg1.channelID && channels[arg0.channelID] && !arg0.member.user.bot) 
+        arg0.guild.channels.cache.get(channels[arg0.channelID]).permissionOverwrites.get(arg0.member.id).delete();
+    if(!arg0.channelID && arg1.channelID && channels[arg1.channelID] && !arg1.member.user.bot)
         arg1.guild.channels.cache.get(channels[arg1.channelID]).createOverwrite(arg1.member.id, {
-            VIEW_CHANNEL: true,
-            SEND_MESSAGES: true,
-            READ_MESSAGE_HISTORY: true
+            VIEW_CHANNEL: true
         });
 })
 
